71de293ba9cc3f20ff6e97d3c0f8b02421d4cd14
Agera K <pruenature@163.com>
2018-09-11 18:11:01 +0800

    init proj

---
+++ app/src/main/java/k/agera/com/identify/MainActivity.kt
@@ -0,0 +1,12 @@
+package k.agera.com.identify
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
